f7dc706e8169f9e3a7dd6d59e9f4d37e4c5e06f6
oal-tena <oal-tena@42.fr>
2022-04-21 11:06:23 +0400

    add print

--- src/main.c
+++ src/main.c
@@ -6,17 +6,13 @@
 /*   By: oal-tena <oal-tena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 19:09:54 by oal-tena          #+#    #+#             */
-/*   Updated: 2022/04/20 13:25:49 by oal-tena         ###   ########.fr       */
+/*   Updated: 2022/04/21 11:06:09 by oal-tena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	main(int argc, char **argv, char **env)
+int	main(void)
 {
-	(void)argc;
-	(void)argv;
-	(void)env;
-	prompt_commend();
-	return (0);
+	printf("ok\n");
 }
