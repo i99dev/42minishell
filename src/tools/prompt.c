ab74e42f583ed818b39e086ce89b11f3707f0646
0x0M4R <omar.abdalla123@gmail.com>
2022-04-21 16:30:25 +0400

    working history

--- src/tools/prompt.c
+++ src/tools/prompt.c
@@ -24,6 +24,7 @@ void	prompt_commend(void)
 
 	user = getenv("USER");
 	host = getenv("HOSTNAME");
+	/*
 	ft_putstr_fd(GREEN"[", 1);
 	ft_putstr_fd(MAGENTA, 1);
 	ft_putstr_fd(user, 1);
@@ -32,10 +33,12 @@ void	prompt_commend(void)
 	ft_putstr_fd(host, 1);
 	ft_putstr_fd(GREEN"]", 1);
 	ft_putstr_fd(GREEN"$~ "RESET, 1);
+	*/
 	while (1)
 	{
-		line = readline("Test");
+		line = readline("minishell $~ ");
 		add_history(line);
 		free(line);
+		line = "NULL";
 	}
 }
