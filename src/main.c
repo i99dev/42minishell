99bfd9716c1d9009f142943d31ed67b15d39d0bf
oal-tena <oal-tena@42.fr>
2022-04-21 11:25:06 +0400

    develop functions to read somthing  - close #6

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: oal-tena <oal-tena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 19:09:54 by oal-tena          #+#    #+#             */
-/*   Updated: 2022/04/21 11:21:25 by oal-tena         ###   ########.fr       */
+/*   Updated: 2022/04/21 11:25:02 by oal-tena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	main(void)
 {
 	printf("ok\n");
-	printf("History 02 d\n");
+	printf("History 03d\n");
 	(void)argc;
 	(void)argv;
 	(void)env;
