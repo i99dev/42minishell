00367cd3f7d62c5e695bdf25aed46c66138582e4
i99dev <obaid.altuniji@gmail.com>
2022-04-22 15:56:52 +0000

    fix norminette formtting #10

--- src/tools/execute.c
+++ src/tools/execute.c
@@ -1,31 +1,31 @@
 #include "../../include/minishell.h"
 /*
-    execute command as child procces in order to keep minishell running
-    TODO: support characters like "|", ">", "<", etc.
+	execute command as child procces in order to keep minishell running
+	TODO: support characters like "|", ">", "<", etc.
 */
 
-void execute(char *path, char **command_table)
+void	execute(char *path, char **command_table)
 {
-    pid_t   pid;
-    int     status;
-    struct  rusage ru;
-    char *command;
+	pid_t	pid;
+	int		status;
+	struct	rusage ru;
+	char	*command;
 
-    if (path[0]!='/')
-        command=ft_strjoin("/bin/",path);
-    else
-        command=path;
-    pid=fork();
-    if (pid < 0) 
-    {
-        err_msg("fork failed");
-    }
-    else if (pid == 0) 
-    {
-        execve(command, command_table,NULL);
-        perror("command failed");
-        // ^ This can only be reached if execve fails
-        exit(1);
-    }
-    wait4(pid, &status, 0, &ru);
+	if (path[0] != '/')
+		command = ft_strjoin("/bin/", path);
+	else
+		command = path;
+	pid = fork();
+	if (pid < 0)
+	{
+		err_msg("fork failed");
+	}
+	else if (pid == 0)
+	{
+		execve(command, command_table, NULL);
+		perror("command failed");
+		// ^ This can only be reached if execve fails
+		exit(1);
+	}
+	wait4(pid, &status, 0, &ru);
 }
