08dbf37f49f0e646fe62c103469dc6e640c1b9f9
0x0M4R <omar.abdalla123@gmail.com>
2022-04-22 18:40:41 +0400

    #10 added support for commands without path

--- src/tools/execute.c
+++ src/tools/execute.c
@@ -9,7 +9,12 @@ void execute(char *path, char **command_table)
     pid_t   pid;
     int     status;
     struct  rusage ru;
+    char *command;
 
+    if (path[0]!='/')
+        command=ft_strjoin("/bin/",path);
+    else
+        command=path;
     pid=fork();
     if (pid < 0) 
     {
@@ -17,7 +22,7 @@ void execute(char *path, char **command_table)
     }
     else if (pid == 0) 
     {
-        execve(path, command_table,NULL);
+        execve(command, command_table,NULL);
         perror("command failed");
         // ^ This can only be reached if execve fails
         exit(1);
