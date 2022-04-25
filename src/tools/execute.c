9ad04be9f99827821574140e20460caf72f56cb1
i99dev <obaid.altuniji@gmail.com>
2022-04-25 08:09:33 +0000

    fix error free_exit #10

--- src/tools/execute.c
+++ src/tools/execute.c
@@ -1,10 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execute.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oal-tena <oal-tena@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/25 08:08:02 by oal-tena          #+#    #+#             */
+/*   Updated: 2022/04/25 08:08:02 by oal-tena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../include/minishell.h"
+
+/*
+TODO:this function need refocter and shift to ft_free or use ft_free_minishell
+*/
+void	free_exit(char **s1, char **s2, char ***table)
+{
+	int	i;
+
+	if (*s1)
+		free(*s1);
+	if (*s2)
+		free(*s2);
+	if (*table)
+	{
+		i = 0;
+		while ((*table)[i])
+			free((*table)[i++]);
+		free(*table);
+	}
+	exit(0);
+}
+
 /*
 	execute command as child procces in order to keep minishell running
 	TODO:	support characters like "|", ">", "<", etc.
 			supoort cd,export,unset,env
 */
-
 void	execute(char *path, char ***command_table)
 {
 	pid_t			pid;
